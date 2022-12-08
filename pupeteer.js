57b598a1dd22ceb1a0192cc6fc1f76b47167f769
Ferran <brechyt@gmail.com>
2022-12-08 23:04:00 +0100

    Screenshots changes and multi URL initial changes

--- pupeteer.js
+++ pupeteer.js
@@ -1,24 +1,26 @@
-var myJson =  require('./data/categories.json');
+var categories =  require('./data/categories.json');
 
 (async () => {
   const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  for (const category of categories) {
+    await page.goto(category);
 
-  await page.goto('https://play.google.com/store/apps?hl=es');
+    const url = category.split('/');
+    let urlCategory = url[url.indexOf('category') + 1];
+    urlCategory = urlCategory.split('?')[0];
 
-  await page.screenshot({                      // Screenshot the website using defined options
-    path: "./screenshot.png",                   // Save the screenshot in current directory
-    fullPage: true                              // take a fullpage screenshot
-  });
+    await page.screenshot({                      // Screenshot the website using defined options
+      path: "./screenshots/"+urlCategory+".png",                   // Save the screenshot in current directory
+      fullPage: true                              // take a fullpage screenshot
+    });
 
-  // títulos kcen6d
-  //const asd = await page.evaluate(() => document.querySelector('.kcen6d').innerText)
-  const asd = await page.$$eval('.kcen6d', els => els.map(el => el.innerText));
-  console.log(asd);
-
-  console.log(myJson);
+    // títulos kcen6d
+    const asd = await page.$$eval('.kcen6d', els => els.map(el => el.innerText));
+    console.log(asd);
+  }
 
   await browser.close();
 })();
